51816fe4711a685fdbcfaa4be15b3c0c11fd25d3
yaminisaini22 <yaminisaini123@gmail.com>
2024-07-04 16:30:53 +0530

    Time: 50 ms (94.39%), Space: 51.5 MB (21.58%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,27 @@
+//paste krio yaha
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    // Step 1: Create a hashmap to store the value and its index
+    const map = new Map();
+
+    //Step 2: Iterate through the array
+    for(let i = 0; i < nums.length; i++){
+        // Step 3: Calculate the complement of the current element
+        const complement = target - nums[i];
+        // Step 4: Check if the complement exists in the map
+        if(map.has(complement)){
+            // If it exists, return the indices
+            return [map.get(complement), i]
+        }
+
+        // Step 5: If it doesn't exist, add the current element to the map
+        map.set(nums[i], i);
+    }
+
+    // Step 6: If no solution is found, return an empty array (this line is usally never reached because the problem guarantees on solutions)
+    return [];
+};
